daaf4eb59975b357940cad05c1b9414784202837
m <chenhui315503490@126.com>
2017-11-22 17:15:40 +0800

    答题活动 ranking和share页面

--- src/app/sharing-activities/sharing-activities.component.ts
+++ src/app/sharing-activities/sharing-activities.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 
 @Component({
@@ -7,14 +7,11 @@ import {Title} from '@angular/platform-browser';
   styleUrls: ['./sharing-activities.component.scss']
 })
 export class SharingActivitiesComponent implements OnInit {
-  @ViewChild('bigBox') bigBox: ElementRef;
   constructor(private title: Title) {
   }
 
   ngOnInit() {
     this.title.setTitle('活动分享');
-    this.bigBox.nativeElement.style.height = document.body.offsetHeight + 'px';
-
   }
 
 }
