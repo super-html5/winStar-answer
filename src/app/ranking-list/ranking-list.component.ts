a33d67b6c7ce44f75d845f7fa2a769d461c92a65
m <chenhui315503490@126.com>
2017-11-22 17:14:42 +0800

    答题活动 ranking和share页面

--- src/app/ranking-list/ranking-list.component.ts
+++ src/app/ranking-list/ranking-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 @Component({
   selector: 'app-ranking-list',
@@ -6,14 +6,12 @@ import {Title} from '@angular/platform-browser';
   styleUrls: ['./ranking-list.component.scss']
 })
 export class RankingListComponent implements OnInit {
-  @ViewChild('bigBox') bigBox: ElementRef;
 
   constructor(private title: Title) {
   }
 
   ngOnInit() {
     this.title.setTitle('排行榜');
-    this.bigBox.nativeElement.style.height = document.body.offsetHeight + 'px';
   }
 
 }
