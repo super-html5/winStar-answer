a41d92c68dd77649f1355b86f7a942a16d51b94e
Mr.Chen <chenhui315503490@126.com>
2017-11-22 18:25:12 +0800

    答题页面，适配完成

--- src/app/answer-list/answer-list.component.ts
+++ src/app/answer-list/answer-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {flyIn} from '../animationsVariable';
 
-
 @Component({
   selector: 'app-answer-list',
   templateUrl: './answer-list.component.html',
@@ -18,7 +17,17 @@ export class AnswerListComponent implements OnInit {
 
   ngOnInit() {
     this.title.setTitle('加油优惠券');
-    this.bigBox.nativeElement.style.height = document.body.offsetHeight + 'px';
+    this.bigBoxHeight();
+  }
+
+  bigBoxHeight(): void {
+    const a_height = $(window).height();
+    const b_height = $('.bigBox').height();
+    if (b_height >= a_height) {
+      this.bigBox.nativeElement.style.height = (b_height + 50) + 'px';
+    } else {
+      this.bigBox.nativeElement.style.height = a_height + 'px';
+    }
   }
 
 }
