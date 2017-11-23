e6badbc6d866c22e3c228cfffd3198aab8899eae
m <chenhui315503490@126.com>
2017-11-23 13:36:44 +0800

    答题活动 修改领取奖品页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import {IndexComponent} from './index/index.component';
 import {AnswerListComponent} from './answer-list/answer-list.component';
 import {GetComponent} from './get/get.component';
 import {GetSuccessComponent} from './get-success/get-success.component';
-import {AddressComponent} from './address/address.component';
 import {RankingListComponent} from './ranking-list/ranking-list.component';
 import {SharingActivitiesComponent} from './sharing-activities/sharing-activities.component';
 const routes: Routes = [
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path: 'answer', component: AnswerListComponent},
   {path: 'get', component: GetComponent},
   {path: 'success', component: GetSuccessComponent},
-  {path: 'address', component: AddressComponent},
   {path: 'rankingList', component: RankingListComponent},
   {path: 'sharing', component: SharingActivitiesComponent}
 
